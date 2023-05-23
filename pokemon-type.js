0135c0226d604e57f7f57c5f8ae0445f981891df
fuzzyJess <jrundle99@yahoo.com>
2023-05-23 18:30:31 +0100

    completed properties and constructors for Water and GrassTypes

--- pokemon-type.js
+++ pokemon-type.js
@@ -7,17 +7,27 @@ class FireType extends Pokemon {
     constructor(type) {
         super(type)
         this.type = 'fire'
-    }
+    };
 };
 
 class WaterType extends Pokemon {
     isWeakTo = "grass";
     isEffectiveAgainst = "fire";
+
+    constructor(type) {
+        super(type)
+        this.type = 'water'
+    };
 };
 
 class GrassType extends Pokemon {
     isWeakTo = "fire";
     isEffectiveAgainst = "water";
+
+    constructor(type) {
+        super(type)
+        this.type = 'grass'
+    };
 };
 
 module.exports = { FireType, WaterType, GrassType };
